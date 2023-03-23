e06dc71b2f1a50ab889b33d73fe46c00351f64ef
sabrinamaral <sabrinamaral@hotmail.com>
2023-03-23 12:50:53 -0300

    app
    basic setup

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("task manager APP");
+});
+app.use("/api/v1/tasks", tasks);
+const port = 3000;
+
+app.listen(port, console.log(`server listening on port ${port}`));
