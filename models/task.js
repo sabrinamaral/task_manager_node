d9de4404c68298920e349baaf5d6dba73bab1930
sabrinamaral <sabrinamaral@hotmail.com>
2023-03-25 11:22:38 -0300

    model validation

--- models/task.js
+++ models/task.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "must provide a name"],
+    trim: true,
+    maxlength: [20, "name cannot be more than 20"],
+    minlength: [4, "name cannot be less than 4"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", taskSchema);
