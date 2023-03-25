632d0622dca5fbe2bad0753580c79c7f9cf71d1d
sabrinamaral <sabrinamaral@hotmail.com>
2023-03-24 22:09:16 -0300

    db config

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 // middleware
 app.use(express.json());
@@ -12,4 +14,13 @@ app.get("/hello", (req, res) => {
 app.use("/api/v1/tasks", tasks);
 const port = 3000;
 
-app.listen(port, console.log(`server listening on port ${port}`));
+const start = async () => {
+  try {
+    await connectDB(process.env.CONNECTION_STRING);
+    app.listen(port, console.log(`server listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
