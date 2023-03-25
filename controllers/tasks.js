1a86fce18066d5218a20319e3ade3c14cc4c3cd4
sabrinamaral <sabrinamaral@hotmail.com>
2023-03-24 22:13:37 -0300

    model and controller
    added task model and created the CRUD

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,26 @@
+const Task = require("../models/task");
+
 const getAllTasks = (req, res) => {
   res.send("all items from the controllers");
 };
 
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.json({ task });
+};
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+const updateTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+const deleteTask = (req, res) => {
+  res.send("delete task");
+};
 module.exports = {
   getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
 };
