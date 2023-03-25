0c4296edcb8bde7b00d590bb32bc04ac01b7ca30
sabrinamaral <sabrinamaral@hotmail.com>
2023-03-25 11:24:41 -0300

    createTask and getAllTask
    added try cash block and used mongoose methods for getting and creating tasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,12 +1,21 @@
 const Task = require("../models/task");
 
-const getAllTasks = (req, res) => {
-  res.send("all items from the controllers");
+const getAllTasks = async (req, res) => {
+  try {
+    const allTasks = await Task.find({});
+    res.json({ allTasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(400).json({ msg: error });
+  }
 };
 const getTask = (req, res) => {
   res.json({ id: req.params.id });
